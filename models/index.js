716f3c746eaa5a8032dff4eea5b285c08062fa48
JM-Santi <113136342+JM-Santi@users.noreply.github.com>
2023-01-31 20:11:04 -0500

    first commit

--- models/index.js
+++ models/index.js
@@ -4,14 +4,24 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category, {
+  foriegnKey: 'product_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foriegnKey: 'category_id'
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+});
 
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+});
 module.exports = {
   Product,
   Category,
